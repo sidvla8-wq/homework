8f94746f3db630cd287d14cfee476df45877fe19
sidvla8-wq <sidvla8@gmail.com>
2026-04-11 12:17:11 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -5,8 +5,22 @@ from datetime import datetime
 
 def mask_account_card(input_string: str) -> str:
     '''Обрабатываем строку о счете или карте и маскируем её'''
+    separate = input_string.strip().split()
+    if not separate:
+        return 'Введена пустая строка'
+    number = separate[-1]
+    sys_pay = ' '.join(separate[:-1])
+    if len(number) == 16 and number.isdigit():
+        mask_number = get_mask_card_number(number)
+
+    else:
+        mask_number = get_mask_account(number)
+    return f'}{sys_pay}, {mask_number}'
+
+
+
     pass
 
 def get_date(date_string: str) -> str:
-    ''' '''
+    '''Возфращаем формат даты согласно заданию'''
     pass
