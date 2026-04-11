8df5b3f0f0a2f7dd90b679145ddec3b3b1b64879
sidvla8-wq <sidvla8@gmail.com>
2026-04-11 10:34:46 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -3,4 +3,5 @@ from .masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(input_string: str) -> str:
+    '''Обрабатываем строку о счете или карте и маскируем её'''
     pass
