6e20da1610daecc83d888f967642632ec3e527c4
sidvla8-wq <sidvla8@gmail.com>
2026-04-18 13:02:37 +0300

    fixes

--- src/processing.py
+++ src/processing.py
@@ -2,21 +2,19 @@
 
 from datetime import datetime
 
-def filter_by_state(filt_operation: list[dict], state: str ='EXECUTED') -> list[dict]:
+
+def filter_by_state(filt_operation: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Фильтрует список словарей по state"""
-    return [f for f in filt_operation if f.get('state') == state]
+    return [f for f in filt_operation if f.get("state") == state]
 
 
 def sort_by_date(filt_operation: list[dict], reverse: bool = True) -> list[dict]:
-    '''Функция сортировки по дате'''
+    """Функция сортировки по дате"""
+
     def sort_date(date_s: str) -> datetime:
         try:
-            return datetime.strptime(date_s, '%Y-%m-%d').date()
+            return datetime.strptime(date_s, "%Y-%m-%d").date()
         except ValueError:
             return datetime.min
 
-    return sorted(
-        filt_operation,
-        key=lambda x: sort_date(x.get('date', '')),
-        reverse=reverse
-    )
+    return sorted(filt_operation, key=lambda x: sort_date(x.get("date", "")), reverse=reverse)
